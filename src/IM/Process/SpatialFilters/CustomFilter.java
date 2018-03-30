9daf60670cedc5d7453eaee2b88b1283d532e3a9
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2018-03-30 16:29:43 -0300

    Added CustomFilter

---
+++ src/IM/Process/SpatialFilters/CustomFilter.java
@@ -0,0 +1,44 @@
+package IM.Process.SpatialFilters;
+
+import java.awt.image.BufferedImage;
+
+public class CustomFilter implements FilterAlgorithm {
+
+    @Override
+    public BufferedImage applyFilter(Filter filter) {
+        int imgWidth = filter.getBufferedImage().getWidth();
+        int imgHeight = filter.getBufferedImage().getHeight();
+        int[][] kernel = filter.getKernel();
+
+        int maskSize = kernel.length / 2;
+
+        BufferedImage filterResult = new BufferedImage(imgWidth, imgHeight, filter.getBufferedImage().getType());
+
+        // Support to calculate the pixel value
+        int buffer;
+
+        for (int i = 0; i < imgWidth; i++) {
+            for (int j = 0; j < imgHeight; j++) {
+
+                buffer = 0;
+
+                // Ignore edges
+                if (i == 0 || i == imgWidth  - 1 || j == 0 || j == imgHeight - 1) {
+                    filterResult.setRGB(i, j, filter.getBufferedImage().getRGB(i, j));
+                    continue;
+                }
+
+                for(int row = i - maskSize; row <= i + maskSize; row++){
+                    for(int column = j - maskSize; column <= j + maskSize; column++){
+                        buffer += kernel[row][column] * filter.getBufferedImage().getRGB(row, column);
+                    }
+                }
+
+                filterResult.setRGB(i, j, buffer);
+
+            }
+        }
+
+        return filterResult;
+}
+}
--- src/IM/Process/SpatialFilters/Filter.java
+++ src/IM/Process/SpatialFilters/Filter.java
@@ -6,17 +6,20 @@ public class Filter {
 
     public static final int MEAN = 1;
     public static final int MEDIAN = 2;
+    public static final int CUSTOM = 3;
 
     protected int filterM;
     protected int filterN;
+    protected int[][] kernel;
 
     protected FilterAlgorithm filter;
     protected BufferedImage image;
 
-    public Filter(int filterType, int filterM, int filterN, BufferedImage image) {
+    public Filter(int filterType, int filterM, int filterN, BufferedImage image, int[][] kernel) {
         this.filterM = filterM;
         this.filterN = filterN;
         this.image = image;
+        this.kernel = kernel;
 
         switch (filterType) {
             case MEAN:
@@ -24,6 +27,9 @@ public class Filter {
                 break;
             case MEDIAN:
                 filter = new MedianFilter();
+                break;
+            case CUSTOM:
+                filter = new CustomFilter();
                 break;
         }
     }
@@ -42,5 +48,9 @@ public class Filter {
 
     public BufferedImage getBufferedImage() {
         return image;
+    }
+
+    public int[][] getKernel() {
+        return kernel;
     }
 }
