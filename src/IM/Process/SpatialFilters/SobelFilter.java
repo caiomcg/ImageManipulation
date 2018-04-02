48b51b050c6f97b4c996b736b108fa2088836890
caio_guedes <caiomcg@gmail.com>
2018-04-02 20:40:04 -0300

    Optimize Sobel filter

--- src/IM/Process/SpatialFilters/SobelFilter.java
+++ src/IM/Process/SpatialFilters/SobelFilter.java
@@ -20,23 +20,23 @@ public class SobelFilter implements FilterAlgorithm {
         for (int i = 1; i < imgWidth - 1; i++) {
             for (int j = 1; j < imgHeight - 1; j++) {
 
-                int value00 = getGrayScale(new Color(image.getRGB(i - 1, j - 1)));
-                int value01 = getGrayScale(new Color(image.getRGB(i - 1, j)));
-                int value02 = getGrayScale(new Color(image.getRGB(i - 1, j + 1)));
-                int value10 = getGrayScale(new Color(image.getRGB(i, j - 1)));
-                int value11 = getGrayScale(new Color(image.getRGB(i, j)));
-                int value12 = getGrayScale(new Color(image.getRGB(i, j + 1)));
-                int value20 = getGrayScale(new Color(image.getRGB(i + 1, j - 1)));
-                int value21 = getGrayScale(new Color(image.getRGB(i + 1, j)));
-                int value22 = getGrayScale(new Color(image.getRGB(i + 1, j + 1)));
+                int value00 = getGrayScale(image.getRGB(i - 1, j - 1));
+                int value01 = getGrayScale(image.getRGB(i - 1, j));
+                int value02 = getGrayScale(image.getRGB(i - 1, j + 1));
+                int value10 = getGrayScale(image.getRGB(i, j - 1));
+                int value11 = getGrayScale(image.getRGB(i, j));
+                int value12 = getGrayScale(image.getRGB(i, j + 1));
+                int value20 = getGrayScale(image.getRGB(i + 1, j - 1));
+                int value21 = getGrayScale(image.getRGB(i + 1, j));
+                int value22 = getGrayScale(image.getRGB(i + 1, j + 1));
 
-                int gx =  ((-1 * value00) + ( 0 * value01) + ( 1 * value02))
-                        + ((-2 * value10) + ( 0 * value11) + ( 2 * value12))
-                        + ((-1 * value20) + ( 0 * value21) + ( 1 * value22));
+                int gx =  ((-1 * value00) + (value02))
+                        + ((-2 * value10) + (2 * value12))
+                        + ((-1 * value20) + (value22));
 
                 int gy =  ((-1 * value00) + (-2 * value01) + (-1 * value02))
-                        + (( 0 * value10) + ( 0 * value11) + ( 0 * value12))
-                        + (( 1 * value20) + ( 2 * value21) + ( 1 * value22));
+                        //+ (( 0 * value10) + ( 0 * value11) + ( 0 * value12))
+                          + ((value20) + ( 2 * value21) + (value22));
 
                 int g = (int)Math.sqrt((gx * gx) + (gy * gy));
 
@@ -63,10 +63,10 @@ public class SobelFilter implements FilterAlgorithm {
 
     }
 
-    private static int  getGrayScale(Color rgb) {
-        int r = rgb.getRed();
-        int g = rgb.getGreen();
-        int b = rgb.getBlue();
+    private static int  getGrayScale(int rgb) {
+        int r = (rgb >> 16) & 0xFF;
+        int g = (rgb >> 8) & 0xFF;
+        int b = rgb & 0xFF;
 
         return (int)(0.2126 * r + 0.7152 * g + 0.0722 * b);
     }
