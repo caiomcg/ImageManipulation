79c7a3b425539a1dbc4c1d0bbd5f6ba3399c080a
Wesnydy Ribeiro <wesnydy@gmail.com>
2018-04-30 14:30:54 -0300

    Multiple chanel convolution

--- src/IM/Process/Convolution/Conv2D.java
+++ src/IM/Process/Convolution/Conv2D.java
@@ -1,5 +1,6 @@
 package IM.Process.Convolution;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Conv2D {
@@ -19,21 +20,45 @@ public class Conv2D {
 
         BufferedImage convResult = new BufferedImage(imgWidth, imgHeight, image.getType());
 
-        int targetPixelValue;
+        int targetPixelRED;
+        int targetPixelGREEN;
+        int targetPixelBLUE;
+
+        int pixelRGB;
 
         for (int y = maskSize / 2; y < imgWidth - (maskSize/2); y++) {
             for (int x = maskSize / 2; x < imgHeight - (maskSize/2); x++) {
 
-                targetPixelValue = 0;
+                targetPixelRED = 0;
+                targetPixelGREEN = 0;
+                targetPixelBLUE = 0;
 
                 for (int ki = - maskSize / 2; ki <= maskSize / 2; ki++) {
                     for (int kj = - maskSize / 2; kj <= maskSize / 2; kj++) {
-                        targetPixelValue += image.getRGB(y+ki, x+kj) * mask[ki+maskSize/2][kj+maskSize/2];
+                        pixelRGB = image.getRGB(y+ki, x+kj);
+
+                        targetPixelRED += ((pixelRGB >> 16) & 0xFF) * mask[ki+maskSize/2][kj+maskSize/2];
+                        targetPixelGREEN += ((pixelRGB >> 8) & 0xFF) * mask[ki+maskSize/2][kj+maskSize/2];
+                        targetPixelBLUE += (pixelRGB & 0xFF) * mask[ki+maskSize/2][kj+maskSize/2];
                     }
                 }
 
-                convResult.setRGB(y, x, targetPixelValue);
+                if (targetPixelRED < 0)
+                    targetPixelRED = 0;
+                else if (targetPixelRED > 255)
+                    targetPixelRED = 255;
 
+                if (targetPixelGREEN < 0)
+                    targetPixelGREEN = 0;
+                else if (targetPixelGREEN > 255)
+                    targetPixelGREEN = 255;
+
+                if (targetPixelBLUE < 0)
+                    targetPixelBLUE = 0;
+                else if (targetPixelBLUE > 255)
+                    targetPixelBLUE = 255;
+
+                convResult.setRGB(y, x, new Color(targetPixelRED, targetPixelGREEN, targetPixelBLUE).getRGB());
             }
         }
 
